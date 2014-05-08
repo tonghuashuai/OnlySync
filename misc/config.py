bfdc40c57ca0756621eb24bce38a14e554b88cf8
tonghs <tonghuashuai@gmail.com>
2014-05-08 12:59:45 +0800

    fix host

--- misc/config.py
+++ misc/config.py
@@ -13,21 +13,23 @@ SEX_CN = {
     0: "保密"
 }
 
+HOST = "http://sync.tonghs.com"
+
 # 新浪微博
 SINA_APP_KEY = "3250001650" # app key
 SINA_APP_SECRET = "21f7e89d289626838e4cc2d3c60140c7" # app secret
-SINA_CALLBACK_URL = "http://job.tonghs.com:8089/auth/sina_callback" # callback url
+SINA_CALLBACK_URL = "{0}/auth/sina_callback".format(HOST) # callback url
 
 # renren
 REN_APP_KEY = "267824"
 REN_APP_SECRET = "9ccb1b104de449ebb7ab5b5df398ec6d"
-REN_REDIRECT_URI = "http://job.tonghs.com:8089/auth/renren_callback"
+REN_REDIRECT_URI = "{0}/auth/renren_callback".format(HOST)
 REN_SCOPE= ["status_update", "photo_upload", "read_user_status"]
 
 # douban
 DOUBAN_APP_KEY = "05d6fd652e63364b1184f82f8b29a2e1"
 DOUBAN_APP_SECRET = "b9dfa5c1a62eb874"
-DOUBAN_REDIRECT_URI  = "http://job.tonghs.com:8089/auth/douban_callback"
+DOUBAN_REDIRECT_URI  = "{0}/auth/douban_callback".format(HOST)
 DOUBAN_SCOPE = 'douban_basic_common,shuo_basic_r,shuo_basic_w'
 
 
